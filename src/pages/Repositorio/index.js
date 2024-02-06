9cceccc6f5a32f44c1a45abf61b26fb39f21709e
Brunocs1991 <brunocosta.1991@hotmail.com>
2024-02-06 20:14:53 -0300

    view details

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,13 +1,53 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
+import {BackButton, Container, Loading, Owner} from "./styles";
+import api from "../../services/api";
+import {FaArrowLeft} from "react-icons/fa";
 
-const Repositorio = () =>{
+const Repositorio = () => {
 
-    const { repositorio } = useParams();
-    return(
-        <div style={{color: "white"}}>
-            {decodeURIComponent(repositorio)}
-        </div>
+    const {repositorio} = useParams();
+    const [repo, setRepo] = useState({});
+    const [issues, setIssues] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        async function load() {
+            const nomeRepo = decodeURIComponent(repositorio)
+            const [repositorioData, issuesData] = await Promise.all([
+                api.get(`repos/${nomeRepo}`),
+                api.get(`repos/${nomeRepo}/issues`, {
+                    params: {
+                        state: 'open',
+                        per_page: 5
+                    }
+                })
+            ]);
+            setRepo(repositorioData.data)
+            setIssues(issuesData.data)
+            setLoading(false)
+        }
+        load().then().catch()
+    },[repositorio]);
+
+    if(loading){
+        return (
+            <Loading>
+                <h1>Carregando...</h1>
+            </Loading>
+        )
+    }
+    return (
+        <Container>
+            <BackButton to={"/"}>
+                <FaArrowLeft color={"#000000"} size={30}/>
+            </BackButton>
+            <Owner>
+                <img src={repo.owner.avatar_url} alt={repo.owner.login}/>
+                <h1>{repo.name}</h1>
+                <p>{repo.description}</p>
+            </Owner>
+        </Container>
     )
 }
 export default Repositorio
